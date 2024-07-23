6fd0670c6514031b9fedcf94ae1562e66b68881d
abhithakur75 <abhithakur6396@gmail.com>
2024-07-23 16:56:59 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".nav");
+  window.addEventListener("scroll", () => {
+    navbar.classList.toggle("active", window.scrollY > 100);
+  });
+  // Responsive navbar js
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector(".nav-list");
+  const navListLink = document.querySelectorAll(".nav-list-link");
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+  navListLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      navList.classList.toggle("active");
+    });
+  });
+});
+
+document.querySelectorAll('.dropdown-item .icon').forEach(item => {
+  item.addEventListener('click', function() {
+      const parent = this.parentElement;
+      parent.classList.toggle('active');
+  });
+});
